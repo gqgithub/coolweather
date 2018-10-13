d9f9c1647aa63f74b6b884a7e193477a868fa94e
gang <gang@gmail.com>
2018-10-13 15:54:52 +0800

    创建数据库

--- app/src/main/java/com/gq/coolweather/MainActivity.java
+++ app/src/main/java/com/gq/coolweather/MainActivity.java
@@ -2,6 +2,10 @@ package com.gq.coolweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.LitePal;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //创建数据库
+        Button createTable= (Button) findViewById(R.id.create);
+        createTable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LitePal.getDatabase();
+            }
+        });
     }
 }
